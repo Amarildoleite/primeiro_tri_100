23b961694501e273f1ce34d3ec53170b4d06c611
Amarildoleite <128834753+Amarildoleite@users.noreply.github.com>
2024-05-01 11:05:22 -0300

    j

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
 let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo - tempoAtual;
 Let segundos = tempoFinal/1000;
 return segundos;
+}
