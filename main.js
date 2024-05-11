45ebd32a12c3805495d7d8704b347b5a241ca496
Amarildoleite <128834753+Amarildoleite@users.noreply.github.com>
2024-05-10 22:36:54 -0300

    l

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-function atualizaCronometro(){
+function atualizaCronometro() {
 for (let i=0; i<contadores.length;i++) {
     contadores[i].textContent = calculaTempo(tempos[i]); 
     }  
